5538d1804bdc3352efd870452ae6af261258ed35
离披 <lipi@shuise.dev>
2025-12-01 08:31:25 +0800

    更新

--- PluginMaster/Program.cs
+++ PluginMaster/Program.cs
@@ -5,7 +5,7 @@ const string PLUGINS_DIRECTORY = @"K:\repos\URA-Plugins\";
 var plugins = new List<PluginInformation>();
 foreach (var path in Directory.EnumerateFiles(PLUGINS_DIRECTORY, "manifest.json", SearchOption.AllDirectories))
 {
-    if (!path.Contains("bin\\Debug\\net8.0")) continue;
+    if (!path.Contains("bin\\Release\\net8.0")) continue;
     plugins.Add(JsonConvert.DeserializeObject<PluginInformation>(File.ReadAllText(path))!);
 }
 File.WriteAllText("../../../manifests.json", JsonConvert.SerializeObject(plugins, Formatting.Indented));
